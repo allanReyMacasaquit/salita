e742e788795f8d1216b3c4d36e4025c542f77306
allan <allanrey.macasaquit@gmail.com>
2024-10-05 22:14:39 -0400

    added quests and leaderboard

--- app/(main)/leaderboard/page.tsx
+++ app/(main)/leaderboard/page.tsx
@@ -1,4 +1,98 @@
-function LeaderboardPage() {
-	return <div>LeaderboardPage</div>;
+import UserProgress from '@/components/user/UserProgress';
+import Image from 'next/image';
+import FeedWrapper from '@/components/wrapper/FeedWrapper';
+import {
+	getTopTenUsers,
+	getUserProgress,
+	getUserSubscription,
+} from '@/database/queries';
+import { redirect } from 'next/navigation';
+import StickyWrapper from '@/components/wrapper/StickyWrapper';
+import { Separator } from '@/components/ui/separator';
+import { Avatar, AvatarImage } from '@/components/ui/avatar';
+
+async function LeaderboardPage() {
+	const userProgress = await getUserProgress();
+	const userSubscription = await getUserSubscription();
+	const leaderboard = await getTopTenUsers();
+	if (!userProgress || !userProgress.activeCourse) {
+		redirect('/courses');
+	}
+	const isPro = !!userSubscription?.isActive;
+
+	return (
+		<div
+			className='
+			flex 
+			flex-row
+			gap-12
+			px-6
+			max-w-7xl'
+		>
+			<FeedWrapper>
+				<div
+					className='
+						w-full
+						flex	
+						flex-col
+						items-center'
+				>
+					<Image
+						src='/images/sidebar/Leaderboard.svg'
+						alt='Leaderboard'
+						height={100}
+						width={100}
+						priority
+						className='w-56'
+					/>
+					<h1
+						className='
+					text-center
+					font-bold
+					text-3xl'
+					>
+						Top 10 Billboard
+					</h1>
+
+					<p className='text-center text-neutral-500 text-sm lg:text-lg my-3 mb-6'>
+						Climb to the top and showcase your skills! Will you make it to #1?
+					</p>
+					<Separator className='mb-4 h-0.5 rounded-full' />
+					{leaderboard.map((userProgress, index) => (
+						<div
+							className='
+							flex 
+							items-center
+							w-full
+							p-4
+							rounded-2xl
+							hover:bg-gray-200/50'
+							key={userProgress.userId}
+						>
+							<p className='font-bold text-lime-700 mr-4'>{index + 1}</p>
+							<Avatar className='border bg-green-500 h-12 w-12 mr-6'>
+								<AvatarImage
+									className='object-cover'
+									src={userProgress.userImageSrc}
+								/>
+							</Avatar>
+							<p className='font-bold text-neutral-800 flex-1'>
+								{userProgress.userName}
+							</p>
+							<p className='text-muted-foreground'>{userProgress.points} XP</p>
+						</div>
+					))}
+				</div>
+			</FeedWrapper>
+			<StickyWrapper>
+				<UserProgress
+					activeCourse={userProgress.activeCourse}
+					hearts={userProgress.hearts}
+					points={userProgress.points}
+					hasActiveSubscription={isPro}
+				/>
+			</StickyWrapper>
+		</div>
+	);
 }
 export default LeaderboardPage;
--- app/(main)/quest/page.tsx
+++ app/(main)/quest/page.tsx
@@ -1,4 +1,129 @@
-function QuestPage() {
-	return <div>QuestPage</div>;
+import { Progress } from '@/components/ui/progress';
+import UserProgress from '@/components/user/UserProgress';
+import FeedWrapper from '@/components/wrapper/FeedWrapper';
+import StickyWrapper from '@/components/wrapper/StickyWrapper';
+import { getUserProgress, getUserSubscription } from '@/database/queries';
+import { Separator } from '@radix-ui/react-separator';
+import Image from 'next/image';
+import { redirect } from 'next/navigation';
+
+const quests = [
+	{
+		title: 'Earn 20XP',
+		value: 20,
+	},
+	{
+		title: 'Earn 50XP',
+		value: 50,
+	},
+	{
+		title: 'Earn 100XP',
+		value: 100,
+	},
+	{
+		title: 'Earn 500XP',
+		value: 500,
+	},
+	{
+		title: 'Earn 1000XP',
+		value: 1000,
+	},
+];
+async function QuestPage() {
+	const userProgress = await getUserProgress();
+	const userSubscription = await getUserSubscription();
+
+	if (!userProgress || !userProgress.activeCourse) {
+		redirect('/courses');
+	}
+	const isPro = !!userSubscription?.isActive;
+
+	return (
+		<div
+			className='
+			flex 
+			flex-row
+			gap-12
+			px-6
+			max-w-7xl'
+		>
+			<FeedWrapper>
+				<div
+					className='
+						w-full
+						flex	
+						flex-col
+						items-center'
+				>
+					<Image
+						src='/images/sidebar/Quest.svg'
+						alt='Quest'
+						height={100}
+						width={100}
+						priority
+						className='w-40'
+					/>
+					<h1
+						className='
+					text-center
+					font-bold
+					text-3xl'
+					>
+						Quest
+					</h1>
+
+					<p className='text-center text-neutral-500 text-sm lg:text-lg my-3 mb-6'>
+						Embark on your journey! Complete tasks, earn rewards, and unlock new
+						adventures!
+					</p>
+					<Separator className='mb-4 h-0.5 rounded-full' />
+
+					<ul className='w-full'>
+						{quests.map((quest) => {
+							const progress = (userProgress.points / quest.value) * 100;
+							return (
+								<div
+									key={quest.title}
+									className='
+										flex 
+										items-center 
+										w-full 
+										gap-x-4 
+										border-t-2
+										p-4'
+								>
+									<Image
+										src='/images/user/points_1.svg'
+										alt='Points'
+										width={60}
+										height={60}
+										priority
+									/>
+									<div
+										className='
+											flex 
+											flex-col
+											gap-y-2
+											w-full'
+									>
+										<p className='text-neutral-700 font-bold'>{quest.title}</p>
+										<Progress value={progress} className='h-3' />
+									</div>
+								</div>
+							);
+						})}
+					</ul>
+				</div>
+			</FeedWrapper>
+			<StickyWrapper>
+				<UserProgress
+					activeCourse={userProgress.activeCourse}
+					hearts={userProgress.hearts}
+					points={userProgress.points}
+					hasActiveSubscription={isPro}
+				/>
+			</StickyWrapper>
+		</div>
+	);
 }
 export default QuestPage;
