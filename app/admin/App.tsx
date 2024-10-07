1b44d2e1668a2096e4a41fc7de24ede349a637cf
allan <allanrey.macasaquit@gmail.com>
2024-10-06 21:52:04 -0400

    Initialize react-ad-min

---
+++ app/admin/App.tsx
@@ -0,0 +1,7 @@
+'use client';
+import { Admin } from 'react-admin';
+
+const App = () => {
+	return <Admin>App</Admin>;
+};
+export default App;
---
+++ app/admin/page.tsx
@@ -0,0 +1,6 @@
+import App from './App';
+
+function AdminPage() {
+	return <App />;
+}
+export default AdminPage;
