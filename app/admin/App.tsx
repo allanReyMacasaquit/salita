0c2d558cc9f182f666cfcff2d2b808c101919da6
allan <allanrey.macasaquit@gmail.com>
2024-10-07 21:24:50 -0400

    Create the first api source and se up headers

--- app/admin/App.tsx
+++ app/admin/App.tsx
@@ -1,7 +1,17 @@
 'use client';
-import { Admin } from 'react-admin';
+import { Admin, ListGuesser, Resource } from 'react-admin';
+import simpleRestProvider from 'ra-data-simple-rest';
 
+const dataProvider = simpleRestProvider('api');
 const App = () => {
-	return <Admin>App</Admin>;
+	return (
+		<Admin dataProvider={dataProvider}>
+			<Resource
+				name='courses'
+				recordRepresentation='title'
+				list={ListGuesser}
+			/>
+		</Admin>
+	);
 };
 export default App;
--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,5 +1,6 @@
-import App from './App';
+import dynamic from 'next/dynamic';
 
+const App = dynamic(() => import('./App'), { ssr: false });
 function AdminPage() {
 	return <App />;
 }
